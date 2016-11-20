ee4bea49936df7acb3962872f00f1d253701ab7f
josevictor1 <josevictorpatos@gmail.com>
2016-11-20 16:42:58 -0200

    mais algumas funções

--- trabalho.py
+++ trabalho.py
@@ -70,7 +70,39 @@ def balanceamento(lista_demanda,lista_oferta,matriz_coeficientes):
     else:
         adiciona_coluna(matriz_coeficientes)
 
+def diferenca_coluna(matriz_coeficientes):
 
+    primeiro_menor = 0
+    segundo_menor = 0
+    l =[]
+
+    for i in range(2,len(matriz_coeficientes[0])):
+        primeiro_menor = matriz_coeficientes[i][0]
+        for j in range(len(matriz_coeficientes)):
+            if primeiro_menor > matriz_coeficientes[j][i]:
+                segundo_menor = primeiro_menor
+                primeiro_menor = matriz_coeficientes[j][i]
+            elif segundo_menor
+
+
+
+
+def diferenca_linha(matriz_coeficientes):
+
+    primeiro_menor = 0
+    segundo_menor =
+    l = []
+    for i in range(len(matriz_coeficientes)):
+        primeiro_menor = matriz_coeficientes[i][0]
+        segundo_menor = primeiro_menor
+        for j in range(2,len(matriz_coeficientes[0])):
+            if primeiro_menor > matriz_coeficientes[i][j]:
+                segundo_menor = primeiro_menor
+                primeirto_menor = matriz_coeficientes[i][j]
+            elif segundo_menor > matriz_coeficientes[i][j]:
+                segundo_menor = matriz_coeficientes[i][j]
+        l.append(segundo_menor - primeiro_menor)
+    return l
 
 
 
