e60193c27ffbc0fcd50c9c3d812e88d1547887d6
josevictor1 <josevictorpatos@gmail.com>
2016-11-22 22:27:27 -0200

    balanceamento funcionando e testado

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-import trabalho.py
+from trabalho1 import *
+
+
+def main():
+
+    #n = input("Numero de origens: ")
+    #m = input("Numero de destinos: ")
+
+    p = Problema()
+    p.matriz_coeficientes = [[1,2,3,4],[4,3,2,4],[0,2,2,1]]
+    p.matriz_x = [[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+    p.l_oferta = [6,8,10]
+    p.l_demanda = [4,7,6,7]
+
+    #Teste balanceamento coluna OK
+    #p.matriz_coeficientes = [[1,2,3],[4,3,2],[0,2,2]]
+    #p.matriz_x = [[0,0,0],[0,0,0],[0,0,0]]
+    #p.l_demanda = [4,7,6]
+
+    #Teste balanceamento linha OK
+    #p.matriz_coeficientes = [[1,2,3,4],[4,3,2,4]]
+    #p.matriz_x = [[0,0,0,0],[0,0,0,0]]
+    #p.l_oferta = [6,8]
+
+    '''print "Ofertas:"
+    p.l_oferta = le_dados(n)
+    print "Demandas:"
+    p.l_demanda = le_dados(m)
+    print "Matriz de coeficientes:"
+    p.matriz_coeficientes = le_matriz(n,m)
+    print "Matriz de variaveis x"
+    p.matriz_x = le_matriz(n,m)'''
+
+    mostra_quadro(p)
+    balanceamento(p)
+    mostra_quadro(p)
+
+main()
--- trabalho1.py
+++ trabalho1.py
@@ -1,39 +1,31 @@
 
-n = input("Numero de origens: ")
-m = input("Numero de destinos: ")
-e = input("Numero de entrepostos: ")
-
 class Problema(object):
 
-    def _init_(sefl,matriz_coeficientes,matriz_x,l_oferta,l_demanda):
-        self.matriz_coeficientes = matriz_coeficientes
-        self.matriz_x = matriz_x
-        self.l_oferta = l_oferta
-        self.l_demanda = l_demanda
+    def __init__(self):
+        self.matriz_coeficientes = []
+        self.matriz_x = []
+        self.l_oferta = []
+        self.l_demanda = []
 
-
-
-def le_dadosorigem(n):
+def le_dados(n):
 
     l = []
     for i in range(n):
         i = input()
         l.append(i)
+
     return l
 
-def le_dadosdestino(m):
+def le_matriz(n,m):
 
-    l = []
+    mat = []
     for i in range(n):
-        i = input()
-        l.append()
-    return l
+        l = []
+        for j in range(m):
+          l.append(input())
+        mat.append(l)
 
-def le_matriz_coeficientes(matriz_coeficientes):
-
-    for i in matriz_coeficientes:
-        i = input()
-    return matriz_coeficientes
+    return mat
 
 def verifica_balanceamento(lista_demanda,lista_oferta):
 
@@ -42,7 +34,7 @@ def verifica_balanceamento(lista_demanda,lista_oferta):
 
     for i in lista_demanda:
         demanda = demanda + i
-    for i in lista_origem:
+    for i in lista_oferta:
         oferta = oferta + i
 
     if demanda == oferta:
@@ -54,43 +46,42 @@ def verifica_balanceamento(lista_demanda,lista_oferta):
         lista_demanda.append(oferta - demanda)
         return "demanda desbalanceado"
 
-
 def adiciona_linha(matriz_coeficientes):
 
     l = []
     l = [0 for i in range(len(matriz_coeficientes[0]))]
     matriz_coeficientes.append(l)
 
-
 def adiciona_coluna(matriz_coeficientes):
 
     for i in matriz_coeficientes:
         i.append(0)
 
+def balanceamento(problema):
 
-
-def balanceamento(lista_demanda,lista_oferta,matriz_coeficientes):
-
-    verifica = verifica_balanceamento(lista_demanda,lista_oferta)
+    verifica = verifica_balanceamento(problema.l_demanda,problema.l_oferta)
 
     if verifica == "balanceado":
         print "Problema balanceado"
     elif verifica == "oferta desbalanceada":
-        adiciona_linha(matriz_coeficientes)
+        adiciona_linha(problema.matriz_coeficientes)
+        adiciona_linha(problema.matriz_x)
     else:
-        adiciona_coluna(matriz_coeficientes)
+        adiciona_coluna(problema.matriz_coeficientes)
+        adiciona_coluna(problema.matriz_x)
 
 def verifica(l):
+
     for i in l:
         if i != 0:
             return False
     return True
 
-
-
 def cantonoroeste(problema):
+
     linha = 0
     coluna = 0
+
     while(verifica(problema.l_oferta) and verifica(l_demanda)):
         if problema.l_oferta[linha] < problema.l_demanda[coluna]:
             problema.matriz_x[linha][coluna] = problema.l_oferta[linha]
@@ -109,6 +100,29 @@ def cantonoroeste(problema):
             coluna = coluna + 1
             linha = linha + 1
 
+def mostra_quadro(problema):
 
+    print"                       "
+
+    for i in range(len(problema.l_demanda)):
+        print "         ",
+        print i,
+        print "       ",
+
+    print "       Oferta"
+
+    for i in range(len(problema.l_oferta)):
+        print  "         |",
+        for j in range(len(problema.l_demanda)):
+            print "x =",problema.matriz_x[i][j]," " ,"c = ",problema.matriz_coeficientes[i][j],"|",
+        print "       ",problema.l_oferta[i]
+
+    print"Demanda",
+    print " |",
+
+    for i in problema.l_demanda:
+        print "      ",
+        print i,
+        print "      |",
 
 
